5779f09c2bc6f716acdc7aac37933c2a74a7d4a2
jellev00 <jelle.vandriessche@gmail.com>
2023-09-17 22:18:31 +0200

    code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.scss';
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="infoContainer">
+        <h1 className="name">ItsJelle</h1>
+        <p className="subTitle">Beginner Frontend Developer</p>
+        <div className="commingSoon">
+          <p className="text">Comming Soon</p>
+          <ul className="loadingDots">
+            <li className="dots"></li>
+            <li className="dots"> </li>
+            <li className="dots"> </li>
+          </ul>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="Footer">
+      <p className="copyright">ItsJelle © 2023 </p>
+    </div>
+  );
+}
+
+export default Footer;
