71b04cff20fd97974a40d3c230fba07edb2a6df9
jellev00 <37414792+jellev00@users.noreply.github.com>
2023-11-16 14:28:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div className="infoContainer">
         <h1 className="name">ItsJelle</h1>
-        <p className="subTitle">Beginner Frontend Developer</p>
+        <p className="subTitle">Beginner Frontend Developer & Backend Developer</p>
         <div className="commingSoon">
           <p className="text">Comming Soon</p>
           <ul className="loadingDots">
